23159bb46f3e313198d0fbd01b5ea6d5891c9f5d
luisgrvc <lmramirezcova@gmail.com>
2016-06-03 13:29:42 +0200

    First trajectory mobile object

--- visualization/src/moveobject.cpp
+++ visualization/src/moveobject.cpp
@@ -18,11 +18,11 @@
 using namespace grvc::com;
 using namespace grvc::hal;
 
-void updateObject(const std::string & object_blue_cylinder_mov, const std::vector<Vec3>&wplist, int _argc, char **_argv)
+void updateObject(const std::string & object_name, const std::vector<Vec3>&wplist, int _argc, char **_argv)
 {
-    Publisher* wpPub = new Publisher("hal_sample", (object_blue_cylinder_mov + "/hal/go_to_wp").c_str(), _argc, _argv);
+    Publisher* wpPub = new Publisher("hal_sample", (object_name + "/hal/go_to_wp").c_str(), _argc, _argv);
     std::string curState;
-    new Subscriber<std::string>("hal_sample", (object_blue_cylinder_mov + "/hal/state").c_str(), _argc, _argv, [&](const std::string& _str) {
+    new Subscriber<std::string>("hal_sample", (object_name + "/hal/state").c_str(), _argc, _argv, [&](const std::string& _str) {
         curState = _str;
     });
 
@@ -39,7 +39,7 @@ void updateObject(const std::string & object_blue_cylinder_mov, const std::vecto
 
 int main(int _argc, char** _argv)
 {
-    std::vector<Vec3>wplist_1 = {{0.0, 1.0, 0.05},{0.0, 3.0, 0.05},{0.0, 5.0, 0.05}};
+    std::vector<Vec3>wplist_1 = {{-50.0, -20.0, 0.07},{50.0, -20.0, 0.07},{50.0, 20.0, 0.07},{-50.0, 20.0, 0.07}, {-50.0, -20.0, 0.07},{50.0, -20.0, 0.07},{50.0, 20.0, 0.07},{-50.0, 20.0, 0.07}, {-50.0, -20.0, 0.07},{50.0, -20.0, 0.07},{50.0, 20.0, 0.07},{-50.0, 20.0, 0.07}};
     //updateObject("/object_blue_cylinder_mov", wplist_1, _argc, _argv);
 
     std::thread object_blue_cylinder_mov(updateObject, "/object_blue_cylinder_mov", wplist_1, _argc, _argv);
