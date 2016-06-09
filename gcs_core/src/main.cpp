097b056732b269cd4073dd951626c293c98fb6d9
Technik <cjtech90@gmail.com>
2016-06-09 13:10:49 +0200

    Fixed demo gcs compilation

--- gcs_core/src/main.cpp
+++ gcs_core/src/main.cpp
@@ -32,22 +32,22 @@ using namespace grvc::com;
 using namespace grvc::hal;
 using namespace std;
 
-sonst string g_node_name = "gcs_core";
+const string g_node_name = "gcs_core";
 
 struct Quadrotor {
 	Quadrotor(const std::string& _quad_name, const WaypointList& _wp_list, int _argc, char** _argv) 
 		: name_(_quad_name)
 		, state_(TaskState::finished)
-		, path_(_wp_list);
+		, path_(_wp_list)
 	{
-		wp_pub_ = new Publisher(g_node_name, name_ + "/hal/go_to_wp", _argc ,_argv);
+		take_off_pub_ = new Publisher(g_node_name, name_ + "/hal/take_off", _argc ,_argv);
 		path_pub_ = new Publisher(g_node_name, name_ + "/hal/path", _argc, _argv);
 
 		new Subscriber<TaskState>(g_node_name, name_ + "/hal/state", _argc, _argv, [this](TaskState _s) {
 			state_ = _s;
 		});
 		
-		new Subscriber<Vec3>(g_node_name_, name_ + "/hal/position", _argc, _argv, [&](const Vec3& _v) {
+		new Subscriber<Vec3>(g_node_name, name_ + "/hal/position", _argc, _argv, [&](const Vec3& _v) {
 			ready_ = true; // Once we receive telemetry, hal is ready
 			cur_z_ = _v.z();
 		});
@@ -57,19 +57,19 @@ struct Quadrotor {
 		assert(path_.size() > 0);
 
 		// Wait for quad to be ready to fly
-		while (!quadIsReady) {
+		while (!ready_) {
 			this_thread::sleep_for(chrono::milliseconds(100));
 		}
 
 		double fly_z = path_[0].pos.z();
-		takeOffPub->publish(fly_z, true);
+		take_off_pub_->publish(fly_z, true);
 		// This is a hack: ROS misses the first few state messages, so we have to check manually.
 		while (cur_z_ < fly_z) {
 			this_thread::sleep_for(chrono::milliseconds(100));
 		}
 
 		for (;;) {
-			path_pub->publish(path_);
+			path_pub_->publish(path_);
 			waitToFinish();
 		}
 	}
@@ -86,7 +86,7 @@ private:
 	}
 
 private:
-	Publisher* wp_pub_;
+	Publisher* take_off_pub_;
 	Publisher* path_pub_;
 
 	string name_;
@@ -104,20 +104,67 @@ void quad_thread(const string& _quad_name, const WaypointList& _list, int _argc,
 
 int main(int _argc, char** _argv) {
 
-	std::vector<Vec3>wplist_1 = {{60.0, -25.0, 20},{30.0, -20.0, 20},{0.0, -20.0, 20},{-30.0, -20.0, 20},{-50.0, -20.0, 20},{-50.0, 0.0, 20},{-50.0, 20.0, 20},{-30.0, 20.0, 20},{0.0, 20.0, 20},{30.0, 20.0, 20},{-30.0, 20.0, 20},{50.0, 20.0, 20}};
+	WaypointList wplist_1 = {
+		{{60.0, -25.0, 20}, 0.0},
+		{{30.0, -20.0, 20}, 0.0},
+		{{0.0, -20.0, 20}, 0.0},
+		{{-30.0, -20.0, 20}, 0.0},
+		{{-50.0, -20.0, 20}, 0.0},
+		{{-50.0, 0.0, 20}, 0.0},
+		{{-50.0, 20.0, 20}, 0.0},
+		{{-30.0, 20.0, 20}, 0.0},
+		{{0.0, 20.0, 20}, 0.0},
+		{{30.0, 20.0, 20}, 0.0},
+		{{-30.0, 20.0, 20}, 0.0},
+		{{50.0, 20.0, 20}, 0.0}
+	};
 	//updateQuadrotor("/quad1", wplist_1, _argc, _argv);
 
-	std::thread quad1(quad_thread, "/quad1", wplist_1, _argc, _argv);
+	std::thread quad1([&](){
+		quad_thread("/quad1", wplist_1, _argc, _argv);
+	});
 
-	std::vector<Vec3>wplist_2 = {{60, -15.0, 16},{30, -5.0, 16},{0.0, 0.0, 16},{-30, 10.0, 16},{-50, 20.0, 16},{0, 20.0, 16},{10, 0.0, 16},{30, -5.0, 16},{0.0, 0.0, 16},{-30, 10.0, 16},{-50, 20.0, 16},{0, 20.0, 16},{10, 0.0, 16}};
+	WaypointList wplist_2 = {
+		{{60, -15.0, 16}, 0.0},
+		{{30, -5.0, 16}, 0.0},
+		{{0.0, 0.0, 16}, 0.0},
+		{{-30, 10.0, 16}, 0.0},
+		{{-50, 20.0, 16}, 0.0},
+		{{0, 20.0, 16}, 0.0},
+		{{10, 0.0, 16}, 0.0},
+		{{30, -5.0, 16}, 0.0},
+		{{0.0, 0.0, 16}, 0.0},
+		{{-30, 10.0, 16}, 0.0},
+		{{-50, 20.0, 16}, 0.0},
+		{{0, 20.0, 16}, 0.0},
+		{{10, 0.0, 16}, 0.0}
+	};
 	//updateQuadrotor("/quad2", wplist_2, _argc, _argv);
 
-	std::thread quad2(quad_thread, "/quad2", wplist_2, _argc, _argv);
+	std::thread quad2([&](){
+		quad_thread("/quad2", wplist_2, _argc, _argv);
+	});
 
-	std::vector<Vec3>wplist_3 = {{60.0, -10.0, 18}, {55.0, -10.0, 18},{45.0, -5.0, 18},{35.0, -10.0, 18},{25.0, -5, 18},{20.0, 0.0, 18},{20.0, 5.0, 18},{10.0, -5.0, 18},{0.0, 5.0, 18},{-10.0, 10.0, 18},{-15.0, 10.0, 18},{-25.0, 0.0, 18},{-10.0, -10.0, 18.0}};
+	WaypointList wplist_3 = {
+		{{60.0, -10.0, 18}, 0.0},
+		{{55.0, -10.0, 18}, 0.0},
+		{{45.0, -5.0, 18}, 0.0},
+		{{35.0, -10.0, 18}, 0.0},
+		{{25.0, -5, 18}, 0.0},
+		{{20.0, 0.0, 18}, 0.0},
+		{{20.0, 5.0, 18}, 0.0},
+		{{10.0, -5.0, 18}, 0.0},
+		{{0.0, 5.0, 18}, 0.0},
+		{{-10.0, 10.0, 18}, 0.0},
+		{{-15.0, 10.0, 18}, 0.0},
+		{{-25.0, 0.0, 18}, 0.0},
+		{{-10.0, -10.0, 18.0}, 0.0}
+	};
 	//updateQuadrotor("/quad2", wplist_2, _argc, _argv);
 
-	std::thread quad3(quad_thread, "/quad3", wplist_3, _argc, _argv);
+	std::thread quad3([&](){
+		quad_thread("/quad3", wplist_3, _argc, _argv);
+	});
 
 	quad1.join();
 	quad2.join();
