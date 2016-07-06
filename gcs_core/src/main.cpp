2d9828a272d0ced20b1d1fcc474c6d453557f438
Fran Real <fran.real.perez@gmail.com>
2016-07-06 19:49:55 +0200

    Test service-based uav in simulation

--- gcs_core/src/main.cpp
+++ gcs_core/src/main.cpp
@@ -2,24 +2,24 @@
 // GRVC Quadrotor HAL sample
 //----------------------------------------------------------------------------------------------------------------------
 // The MIT License (MIT)
-// 
+//
 // Copyright (c) 2016 GRVC University of Seville
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
 // documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
 // rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
 // permit persons to whom the Software is furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
 // Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
 // WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
 // OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //----------------------------------------------------------------------------------------------------------------------
 #include <grvc_com/publisher.h>
-#include <grvc_quadrotor_hal/server.h>
+#include <grvc_quadrotor_uav/server.h>
 #include <grvc_quadrotor_hal/types.h>
 #include <grvc_utils/argument_parser.h>
 #include <cstdint>
@@ -31,26 +31,19 @@
 
 using namespace grvc::com;
 using namespace grvc::hal;
+using namespace grvc;
 using namespace std;
 
 const string g_node_name = "gcs_core";
 
 struct Quadrotor {
-	Quadrotor(const std::string& _quad_name, const WaypointList& _wp_list, int _argc, char** _argv) 
+	Quadrotor(const std::string& _quad_name, const WaypointList& _wp_list, int _argc, char** _argv)
 		: name_(_quad_name)
-		, state_(TaskState::finished)
 		, path_(_wp_list)
 	{
-		take_off_pub_ = new Publisher(g_node_name, name_ + "/hal/take_off", _argc ,_argv);
-		path_pub_ = new Publisher(g_node_name, name_ + "/hal/path", _argc, _argv);
-
-		new Subscriber<TaskState>(g_node_name, name_ + "/hal/state", _argc, _argv, [this](TaskState _s) {
-			state_ = _s;
-		});
-		
+		path_srv_ = new grvc::uav::Server::PathService::Client(name_ + "/uav/path", utils::ArgumentParser(_argc, _argv));
 		new Subscriber<Vec3>(g_node_name, name_ + "/hal/position", _argc, _argv, [&](const Vec3& _v) {
 			ready_ = true; // Once we receive telemetry, hal is ready
-			cur_z_ = _v.z();
 		});
 	}
 
@@ -62,39 +55,16 @@ struct Quadrotor {
 			this_thread::sleep_for(chrono::milliseconds(100));
 		}
 
-		double fly_z = path_[0].pos.z();
-		take_off_pub_->publish(fly_z, true);
-		// This is a hack: ROS misses the first few state messages, so we have to check manually.
-		while (cur_z_ < fly_z*0.8) {
-			this_thread::sleep_for(chrono::milliseconds(100));
-		}
+		grvc::uav::ActionState as;
+		path_srv_->send(path_, as);
 
-		for (;;) {
-			path_pub_->publish(path_);
-			waitToFinish();
-		}
 	}
 
 private:
-	// Complete current task
-	void waitToFinish() {
-		while (state_ != TaskState::running) {
-			this_thread::sleep_for(chrono::milliseconds(100));
-		}
-		while (state_ != TaskState::finished) {
-			this_thread::sleep_for(chrono::milliseconds(100));
-		}
-	}
-
-private:
-	Publisher* take_off_pub_;
-	Publisher* path_pub_;
+	grvc::uav::Server::PathService::Client* path_srv_;
 
 	string name_;
-	TaskState state_;
 	WaypointList path_;
-
-	double cur_z_;
 	bool ready_ = false;
 };
 
@@ -104,7 +74,7 @@ void quad_thread(const string& _quad_name, const WaypointList& _list, int _argc,
 }
 
 int main(int _argc, char** _argv) {
-	grvc::utils::ArgumentParser parser(_argc, _argv);
+	utils::ArgumentParser parser(_argc, _argv);
 	string quad = parser.getArgument("quad_name", string("quad1"));
 
 	if(quad == "quad1") {
@@ -141,7 +111,7 @@ int main(int _argc, char** _argv) {
 			{{0, 20.0, 16}, 0.0},
 			{{10, 0.0, 16}, 0.0}
 		};
-		
+
 		quad_thread("/quad2", wplist_2, _argc, _argv);
 	}
 
@@ -161,7 +131,7 @@ int main(int _argc, char** _argv) {
 			{{-25.0, 0.0, 18}, 0.0},
 			{{-10.0, -10.0, 18.0}, 0.0}
 		};
-	
+
 		quad_thread("/quad3", wplist_3, _argc, _argv);
 	}
 
