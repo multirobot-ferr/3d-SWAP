7e9515e3cbe730674df9c423a62ac3b344eb72c6
luisgrvc <lmramirezcova@gmail.com>
2016-07-12 13:12:34 +0200

    Caught static object

--- visualization/src/take_object.cpp
+++ visualization/src/take_object.cpp
@@ -2,60 +2,51 @@
 #include <tf/transform_broadcaster.h>
 #include <tf/transform_listener.h>
 #include <stdio.h>
+#include <iostream>
 #include <std_msgs/String.h>
+#include <grvc_quadrotor_hal/types.h>
+#include <grvc_quadrotor_hal/server.h>
 #include <string.h>
+#include <thread>
+#include <chrono>
 
 
 using namespace std;
+using namespace grvc::hal;
 
-/*void chatterCallback(const std_msgs::String::ConstPtr& msg)
-{
-    //ROS_INFO("[%s]", msg->data.c_str());
+Vec3 g_piecePos;
+const double cCatchThreshold = 0.5f;
+bool caught = false;
+Server::WaypointService::Client* waypoint_srv;
+int i = 0;
+void catchCallback(const Vec3& _pos) {
 
-    int i = 0;
-    char * str =(char*) msg->data.c_str();
-    char * pch = strtok(str,"\n");
-    string pos[3];
-
-    while(pch != NULL)
-    {
-        //printf("%s\n",pch);
-        pos[i++].append(pch);
-        pch = strtok (NULL, "\n");
-
+    if((_pos - g_piecePos).norm() < cCatchThreshold ) {
+        if(!caught) cout << "Caught! -----------------\n";
+        caught = true;
     }
-
-    for(i=0; i<3; i++)
-    {
-        printf("%s\n",pos[2].c_str());
+    if(caught) {
+        g_piecePos = _pos + Vec3(0.0, 0.0, -0.2);
     }
+}
 
-}*/
-
-
-int main(int argc, char** argv)
+int main(int _argc, char** _argv)
 {
-    ros::init(argc, argv, "take_object");
-    ros::NodeHandle n;
+    string node_name = "take_object";
+    grvc::utils::ArgumentParser args(_argc, _argv);
+    g_piecePos = args.getArgument("start_pos", g_piecePos);
+    cout << g_piecePos << endl;
 
-   // ros::Subscriber sub = n.subscribe("/quad1/hal/position", 1000, chatterCallback);
-    tf::TransformBroadcaster br;
-    tf::Transform transform;
+    waypoint_srv = new Server::WaypointService::Client(args.getArgument("gazebo_ns", string(""))+"/hal/go_to_wp", args);
 
-    ros::Rate rate(10);
+    new grvc::com::Subscriber<Vec3>(node_name.c_str(), "/quad1/hal/position", _argc, _argv, catchCallback);
 
-    while(n.ok())
-    {
-        transform.setOrigin(tf::Vector3(0.0, 0.1, 0.0));
-        transform.setRotation(tf::Quaternion(0, 0, 0, 1));
-        br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "base_link_1", "base_link_4"));
-
-        rate.sleep();
+    for(;;) {
+        TaskState ts;
+        Waypoint wp = {g_piecePos, 0.0};
+        waypoint_srv->send(wp, ts);
+        this_thread::sleep_for(chrono::milliseconds(100));
     }
 
     return 0;
-
-   // ros::spin();
-
-    //return 0;
 }
