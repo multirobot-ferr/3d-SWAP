da01dca6458cb4d28ba0371129c80cc3400d79ef
luisgrvc <lmramirezcova@gmail.com>
2016-07-13 16:49:02 +0200

    Pick up static object

--- visualization/src/take_object.cpp
+++ visualization/src/take_object.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
 #include <tf/transform_listener.h>
+#include <gazebo_msgs/ModelState.h>
 #include <stdio.h>
 #include <iostream>
 #include <std_msgs/String.h>
@@ -10,42 +11,66 @@
 #include <thread>
 #include <chrono>
 
-
 using namespace std;
 using namespace grvc::hal;
 
-Vec3 g_piecePos;
-const double cCatchThreshold = 0.7f;
-bool caught = false;
+
+
 Server::WaypointService::Client* waypoint_srv;
 int i = 0;
-void catchCallback(const Vec3& _pos) {
 
-    if((_pos - g_piecePos).norm() < cCatchThreshold ) {
-        if(!caught) cout << "Caught! -----------------\n";
-        caught = true;
+class ObjectHaunter {
+public:
+    ObjectHaunter(int _argc, char** _argv) {
+        string node_name = "take_object";
+        grvc::utils::ArgumentParser args(_argc, _argv);
+        piecePos_ = args.getArgument("start_pos", piecePos_);
+        //cout << piecePos_ << endl;
+        ros::init(_argc, _argv, "haunter");
+        n = new ros::NodeHandle();
+        modelStatePub_ = n->advertise<gazebo_msgs::ModelState>("/gazebo/set_model_state", 100);
+        robotPosSub_ = new grvc::com::Subscriber<Vec3>(node_name.c_str(), "/quad1/hal/position", _argc, _argv, [this](const Vec3& _pos) {
+            catchCallback(_pos);
+        });
     }
-    if(caught) {
-        g_piecePos = _pos + Vec3(0.0, 0.0, -0.2);
+
+protected:
+    void catchCallback(const Vec3& _pos) {
+
+        if((_pos - piecePos_).norm() < cCatchThreshold_ ) {
+            if(!caught_) cout << "Caught! -----------------\n";
+            caught_ = true;
+        }
+        if(caught_) {
+            piecePos_ = _pos + Vec3(0.0, 0.0, -0.2);
+            gazebo_msgs::ModelState targetPos;
+            targetPos.model_name = "object_black_cylinder_1";
+            targetPos.pose.position.x = piecePos_[0];
+            targetPos.pose.position.y = piecePos_[1];
+            targetPos.pose.position.z = piecePos_[2];
+            modelStatePub_.publish(targetPos);
+        }
     }
-}
+
+    Vec3 piecePos_;
+    const double cCatchThreshold_ = 0.7f;
+    bool caught_ = false;
+    ros::NodeHandle* n;
+    ros::Publisher modelStatePub_;
+    grvc::com::Subscriber<Vec3>* robotPosSub_;
+
+};
+
+
+
 
 int main(int _argc, char** _argv)
 {
-    string node_name = "take_object";
-    grvc::utils::ArgumentParser args(_argc, _argv);
-    g_piecePos = args.getArgument("start_pos", g_piecePos);
-    cout << g_piecePos << endl;
 
-    waypoint_srv = new Server::WaypointService::Client(args.getArgument("gazebo_ns", string(""))+"/hal/go_to_wp", args);
-
-    new grvc::com::Subscriber<Vec3>(node_name.c_str(), "/quad1/hal/position", _argc, _argv, catchCallback);
+    ObjectHaunter haunter(_argc, _argv);
 
     for(;;) {
-        TaskState ts;
-        Waypoint wp = {g_piecePos, 0.0};
-        waypoint_srv->send(wp, ts);
-        this_thread::sleep_for(chrono::milliseconds(100));
+         this_thread::sleep_for(chrono::milliseconds(500));
     }
 
     return 0;
