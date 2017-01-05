95a2d492e921c68e049721bab174585a9b1086f9
luisgrvc <lmramirezcova@gmail.com>
2017-01-05 13:30:54 +0100

    Added control in altitude to avoid drifts

--- uav_visual_servoing/src/main.cpp
+++ uav_visual_servoing/src/main.cpp
@@ -40,11 +40,17 @@
 #include <gcsLink.h>
 #include <CandidateList.h>
 #include <Candidate.h>
+#include <std_msgs/Float64.h>
+
 
 using namespace grvc;
 using namespace std;
 
 void candidateCallback(const std_msgs::String::ConstPtr _msg);
+float gAltitude = 15;
+void altitudeCallback(const std_msgs::Float64::ConstPtr _msg){
+    gAltitude = _msg->data;
+}
 mbzirc::Candidate bestCandidateMatch(const mbzirc::CandidateList &_list, const mbzirc::Candidate &_specs);
 
 hal::Server::PositionErrorService::Client *pos_error_srv;
@@ -56,6 +62,13 @@ int main(int _argc, char** _argv){
     // Init services.
     grvc::utils::ArgumentParser args(_argc, _argv);
     pos_error_srv = new hal::Server::PositionErrorService::Client("/mbzirc_1/hal/pos_error", args);
+
+    ros::Subscriber altitudeSubs;
+    ros::NodeHandle nh;
+    if(ros::isInitialized()){
+        altitudeSubs = nh.subscribe<std_msgs::Float64>("/mavros_1/global_position/rel_alt", 10, altitudeCallback);
+    }
+    
 
     hal::Server::TakeOffService::Client takeOff_srv("/mbzirc_1/hal/take_off", args);
     hal::TaskState ts;
@@ -69,8 +82,6 @@ int main(int _argc, char** _argv){
     std::this_thread::sleep_for(std::chrono::seconds(5));
     std::cout << "Connected to hal" << std::endl;
 
-
-    ros::NodeHandle nh;
     mbzirc::CandidateList candidateList;
     ros::Subscriber candidateSubscriber = nh.subscribe<std_msgs::String>("/candidateList", 1, candidateCallback);
 
@@ -102,6 +113,7 @@ void candidateCallback(const std_msgs::String::ConstPtr _msg){
     if(candidateList.candidates.size() > 0){
         mbzirc::Candidate target = bestCandidateMatch(candidateList, specs);
         std::cout << "tracking candidate with error " << target.location.transpose() << std::endl;
+        target.location[2] = 15-gAltitude;
         pos_error_srv->send(target.location, ts);
 
     }else{
