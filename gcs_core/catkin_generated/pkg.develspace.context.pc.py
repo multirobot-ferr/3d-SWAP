97de2e77ad20c4977bf44145f0f714a1a4b63ef2
jcapitan <jcapitan@us.es>
2017-02-07 13:11:41 +0100

    catkin_make and cmake files not needed removed from repository.

--- gcs_core/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "grvc_quadrotor_com;grvc_quadrotor_hal;grvc_quadrotor_uav".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "mbzirc_gcs_core"
-PROJECT_SPACE_DIR = "/home/luisgrvc/catkin_ws/src/devel"
-PROJECT_VERSION = "0.0.0"
