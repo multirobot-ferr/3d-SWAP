c3d4bfe8dc0f281daef3b74ebb43291ba40152b7
Fran Real <fran.real.perez@gmail.com>
2017-03-04 21:34:33 +0100

    Test successfully in gazebo simulation, position only animation

--- visualization/src/gazebo_move.cpp
+++ visualization/src/gazebo_move.cpp
@@ -1,11 +1,18 @@
 #include <list>
 #include <iterator>
+#include <chrono>
+#include <thread>
 #include <ros/ros.h>
-//#include <gazebo_msgs/LinkState.h>
+#include <geometry_msgs/Point.h>
+#include <gazebo_msgs/LinkState.h>
 
 struct Frame {
-    Frame(double _test) { test = _test; }
-    double test;
+    Frame(double _x, double _y, double _z) {
+        position.x = _x;
+        position.y = _y;
+        position.z = _z;
+     }
+    geometry_msgs::Point position;
 };
 
 struct KeyFrame {
@@ -22,7 +29,10 @@ struct KeyFrame {
         double t_next = _next.time.toSec();
         double t_curr = _elapsed.toSec();
         double t = (t_curr - t_prev) / (t_next - t_prev);
-        return Frame((1.0 - t) * _prev.frame.test + t * _next.frame.test);
+        double x = (1.0 - t) * _prev.frame.position.x + t * _next.frame.position.x;
+        double y = (1.0 - t) * _prev.frame.position.y + t * _next.frame.position.y;
+        double z = (1.0 - t) * _prev.frame.position.z + t * _next.frame.position.z;
+        return Frame(x, y, z);
     }
 };
 
@@ -31,8 +41,8 @@ public:
     GazeboAnimatedLink(const std::string& _link_name, const std::string& _reference_frame = "map") {
         link_name_ = _link_name;
         reference_frame_ = _reference_frame;
-        //std::string link_state_pub_topic = "/gazebo/set_link_state";
-        //link_state_publisher_ = nh_.advertise<gazebo_msgs::LinkState>(link_state_pub_topic, 1);
+        std::string link_state_pub_topic = "/gazebo/set_link_state";
+        link_state_publisher_ = nh_.advertise<gazebo_msgs::LinkState>(link_state_pub_topic, 1);
     }
 
     void addKeyFrame(const KeyFrame& _key_frame) {
@@ -47,10 +57,15 @@ public:
         playing_ = true;
         key_frames_.sort();
         std::list<KeyFrame>::iterator next_frame = key_frames_.begin();
+        // Wait for simulation time initialization
+        while (ros::Time::now() == ros::Time(0)) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
         ros::Time play_started = ros::Time::now();
         ros::Rate rate(_fps);
         while (playing_ && ros::ok()) {
             ros::Duration elapsed = ros::Time::now() - play_started;
+            //ROS_INFO("elapsed = %f", elapsed.toSec());
             while (next_frame->time.toSec() < elapsed.toSec()) {
                 next_frame++;
                 if (next_frame == key_frames_.end()) {
@@ -64,7 +79,14 @@ public:
                 auto prev_frame = std::prev(next_frame);
                 curr_frame = KeyFrame::interpolate(*prev_frame, *next_frame, elapsed);
             }
-            ROS_INFO("test = %f", curr_frame.test);
+            gazebo_msgs::LinkState current;
+            current.link_name = link_name_;
+            current.pose.position.x = curr_frame.position.x;
+            current.pose.position.y = curr_frame.position.y;
+            current.pose.position.z = curr_frame.position.z;
+            current.reference_frame = reference_frame_;
+            link_state_publisher_.publish(current);
+            ros::spinOnce();
             rate.sleep();
         }
     }
@@ -83,10 +105,10 @@ protected:
 int main(int _argc, char** _argv) {
     ros::init(_argc, _argv, "gazebo_move");
     ROS_INFO("Starting gazebo_move");
-    GazeboAnimatedLink yellow("Yellow cylinder object::grab_here");
-    yellow.addKeyFrame(KeyFrame(10.0, ros::Time(10.0)));
-    yellow.addKeyFrame(KeyFrame(0.0, ros::Time(5.0)));
-    yellow.addKeyFrame(KeyFrame(15.0, ros::Time(15.0)));
+    GazeboAnimatedLink yellow("Yelow cylinder object::grab_here");  // WATCHOUT: misspelled!
+    yellow.addKeyFrame(KeyFrame(Frame(0.0, 0.0, 0.0), ros::Time(0.1)));
+    yellow.addKeyFrame(KeyFrame(Frame(1.0, 1.0, 1.0), ros::Time(5.0)));
+    yellow.addKeyFrame(KeyFrame(Frame(0.0, 0.0, 0.0), ros::Time(10.0)));
     yellow.play(25);
     return 0;
 }
