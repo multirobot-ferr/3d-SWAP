6e2eb2469ead5ff26c8a498949f6e1f50570d70c
Eduardo Ferrera <eduardo.ferrera@uni-due.de>
2017-11-15 16:02:23 +0100

    New altitude on the uavs and matlab scripts to process files

--- uav_avoidance/logs/experiment01/ComputePositionError.m
+++ uav_avoidance/logs/experiment01/ComputePositionError.m
@@ -1,14 +1,14 @@
-function [ output_args ] = ComputePositionError( input_args )
+function ComputePositionError()
 %COMPUTEPOSITIONERROR This script computes the error in position of an UAV
 %that remains static in a position
 
 % Loading the log files
-data=load(input_args);
+data=load('uav2.txt');
 
 % Extracting the number of measurements
 mess = size(data,1);
 
-measurements = data(:,1:2)';
+measurements = data(:,4:5)';
 
  
 % Computing the simple mean
@@ -28,11 +28,20 @@ plot(measurements(1,:), measurements(2,:), '.r', ...
      p_mean(1), p_mean(2), 'or', ...
      p_mean_no_out(1), p_mean_no_out(2), 'ob')
 legend('Measurements Discarted', 'Measurements', 'Mean', 'Mean no outlayers')
+
+
+errors_2 = sqrt((p_mean_no_out(1) - measurements(1,:)).^2 + (p_mean_no_out(2) - measurements(2,:)).^2);
+disp(['Mean at (' num2str(p_mean) '), error in mean ' num2str(max(errors))])
+disp(['Mean at (' num2str(p_mean) '), error in mean no outlayers ' num2str(max(errors_2))])
+
+err_x = (max(errors))*cos(-pi:0.2:pi) + p_mean(1);
+err_y = (max(errors))*sin(-pi:0.2:pi) + p_mean(2);
+
+hold on
+plot(err_x, err_y, '-r')
+
+err_x = (max(errors_2))*cos(-pi:0.1:pi) + p_mean_no_out(1);
+err_y = (max(errors_2))*sin(-pi:0.1:pi) + p_mean_no_out(2);
+plot(err_x, err_y, '-b')
  
-disp(['Mean at (' num2str(p_mean) '), error in mean ' num2str(sqrt(sum((p_mean - p).^2)))])
-disp(['Mean at (' num2str(p_mean) '), error in mean no outlayers ' num2str(sqrt(sum((p_mean_no_out - p).^2)))])
- 
-
-
-
 end
